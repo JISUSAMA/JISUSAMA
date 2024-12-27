3b5c7ad138404e1afd3cb3c97d2b1853ec7f8002
JISUSAMA <j2su0218@gmail.com>
2024-12-27 22:07:35 +0900

    Update readmeUpdate.js

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -140,4 +140,23 @@ const parser = new Parser({
   });
 
   console.log("업데이트 완료");
+})(async () => {
+  // 피드 목록
+  const feed = await parser.parseURL("https://blog.naver.com/j2su0218/rss");
+
+  // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
+  for (let i = 0; i < 5; i++) {
+    const { title, link } = feed.items[i];
+    console.log(`${i + 1}번째 게시물`);
+    console.log(`추가될 제목: ${title}`);
+    console.log(`추가될 링크: ${link}`);
+    text += `<a href=${link}>${title}</a></br>`;
+  }
+
+  // README.md 파일 작성
+  writeFileSync("README.md", text, "utf8", (e) => {
+    console.log(e);
+  });
+
+  console.log("업데이트 완료");
 })();
